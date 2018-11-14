531c0b8333f448dcc1b96b06d5f3ac807a983b99
HANAI tohru <tohru@reedom.com>
2018-11-14 12:52:54 +0900

    chore: add auto generated code for flutter package development

---
+++ ios/Classes/FlutterAppnextPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterAppnextPlugin.h"
+
+@implementation FlutterAppnextPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_appnext"
+            binaryMessenger:[registrar messenger]];
+  FlutterAppnextPlugin* instance = [[FlutterAppnextPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
